7b5bdf851508db966225e23324ae315d46cf9bc0
tosainu27 <tosa_inu@mail.ru>
2023-12-16 16:23:56 +0700

    init: first task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -7,19 +7,19 @@
  *                                                                                           *
  ******************************************************************************************* */
 
-/**
- * Returns an area of a rectangle given by width and height.
- *
- * @param {number} width
- * @param {number} height
- * @return {number}
- *
- * @example:
- *   5, 10 => 50
- *   5, 5  => 25
- */
-function getRectangleArea(/* width, height */) {
-  throw new Error('Not implemented');
+// /**
+//  * Returns an area of a rectangle given by width and height.
+//  *
+//  * @param {number} width
+//  * @param {number} height
+//  * @return {number}
+//  *
+//  * @example:
+//  *   5, 10 => 50
+//  *   5, 5  => 25
+//  */
+function getRectangleArea(width, height) {
+  return Number(width * height);
 }
 
 /**
