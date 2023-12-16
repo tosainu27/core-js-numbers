32802d50b87034decfb6310646e5fb279dff5502
tosainu27 <tosa_inu@mail.ru>
2023-12-16 17:57:51 +0700

    feat: 4 tasks

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -7,69 +7,20 @@
  *                                                                                           *
  ******************************************************************************************* */
 
-// /**
-//  * Returns an area of a rectangle given by width and height.
-//  *
-//  * @param {number} width
-//  * @param {number} height
-//  * @return {number}
-//  *
-//  * @example:
-//  *   5, 10 => 50
-//  *   5, 5  => 25
-//  */
 function getRectangleArea(width, height) {
   return Number(width * height);
 }
 
-/**
- * Returns a circumference of circle given by radius.
- *
- * @param {number} radius
- * @return {number}
- *
- * @example:
- *   5    => 31.41592653589793
- *   3.14 => 19.729201864543903
- *   0    => 0
- */
-function getCircleCircumference(/* radius */) {
-  throw new Error('Not implemented');
+function getCircleCircumference(radius) {
+  return Math.PI * 2 * radius;
 }
 
-/**
- * Returns an average of two given numbers.
- *
- * @param {number} value1
- * @param {number} value2
- * @return {number}
- *
- * @example:
- *   5, 5  => 5
- *  10, 0  => 5
- *  -3, 3  => 0
- */
-function getAverage(/* value1, value2 */) {
-  throw new Error('Not implemented');
+function getAverage(value1, value2) {
+  return value1 / 2 + value2 / 2;
 }
 
-/**
- * Returns a distance between two points by cartesian coordinates.
- *
- * @param {number} x1
- * @param {number} y1
- * @param {number} x2
- * @param {number} y2
- *
- * @return {number}
- *
- * @example:
- *   (0,0) (0,1)    => 1
- *   (0,0) (1,0)    => 1
- *   (-5,0) (10,-10) => 18.027756377319946
- */
-function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getDistanceBetweenPoints(x1, y1, x2, y2) {
+  return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
 }
 
 /**
